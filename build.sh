0739f3551243a1f0f8745c2236a8e5e376f3a9b9
Fiaguhop137 <eastonzou@gmail.com>
2026-08-30 17:28:35 -0400

    Auto-commit: 2026.08.30.17.28.35

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ g++ -std=c++17 assets/bundler.cpp
 cosmoc++ -std=c++23 -O3 -g -Wall -Wextra -Wpedantic -Wshadow -Wconversion -fdiagnostics-color=always confluxia.cpp -o confluxia
 nohup git sync > /dev/null 2>&1 &
 rm -f a.out
+rm -f confluxia.aarch64.elf
+rm -f confluxia.com.dbg
