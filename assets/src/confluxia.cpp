cd16b3c4890a0a0da20cc9bdbcb3c804b68491a3
Fiaguhop137 <eastonzou@gmail.com>
2026-08-31 08:29:00 -0400

    Auto-commit: 2026.08.31.08.29.00

--- assets/src/confluxia.cpp
+++ assets/src/confluxia.cpp
@@ -69,28 +69,28 @@ const unordered_map<string,move>moves=load_moves();
 const unordered_map<string,pet>pets=load_pets();
 const unordered_map<string,string>lore_defs=load_lore();
 const unordered_map<string,vector<string>> modifiers=load_modifiers();
-void typewrite(string text){
+void print(string text){
     for(size_t i=0,len=text.size();i<len;++i){
         cout<<text[i]<<std::flush;
         std::this_thread::sleep_for(std::chrono::milliseconds(30));
     }
 }
-string user_input(string prompt,vector<string>valid_options={}){
+string input(string prompt,vector<string>valid_options={}){
     vector<string> printed_options=valid_options;
     while(true){
         string response;
-        typewrite(prompt);
-        typewrite(" ");
+        print(prompt);
+        print(" ");
         if(!valid_options.empty()){
             if(printed_options==vector<string>{"y/n"}){
                 valid_options={"yes","no","y","n"};
             }
-            typewrite("(");
+            print("(");
             for(size_t i=0;i<printed_options.size()-1;i++){
-                typewrite(printed_options[i]);
-                if(i<printed_options.size()-2){typewrite(", ");}
+                print(printed_options[i]);
+                if(i<printed_options.size()-2){print(", ");}
             }
-            typewrite(" or "+printed_options.back()+") ");
+            print(" or "+printed_options.back()+") ");
         }
         std::getline(cin,response);
         string response_lower=response;
@@ -107,14 +107,14 @@ string user_input(string prompt,vector<string>valid_options={}){
                 }
             }
             if(!valid){
-                typewrite("Invalid input. Please choose ");
+                print("Invalid input. Please choose ");
                 for(size_t i=0;i<printed_options.size()-1;i++){
-                    typewrite(printed_options[i]);
+                    print(printed_options[i]);
                     if(i<printed_options.size()-2){
-                        typewrite(", ");
+                        print(", ");
                     }
                 }
-                typewrite(" or "+printed_options.back()+". ");
+                print(" or "+printed_options.back()+". ");
             }else{
                 if(valid_options==vector<string>{"yes","no","y","n"}){
                     if(response_lower=="y"){response="yes";}
@@ -160,8 +160,8 @@ struct player{
     stat_block stats{10,10,10,100};
     power_construct powers;
     player(){
-        name=user_input("What would you like to name your character?");
-        bool randomize=user_input("Would you like to randomize your character's stats and powers?",{"y/n"})=="yes";
+        name=input("What would you like to name your character?");
+        bool randomize=input("Would you like to randomize your character's stats and powers?",{"y/n"})=="yes";
         if(randomize){
             vector<string> basic_options={"fire","metal","wood","earth","water","fire","metal","wood","earth","water","nexus"};
             std::uniform_int_distribution<size_t> basic_dist(0,basic_options.size()-1);
@@ -173,9 +173,9 @@ struct player{
             std::uniform_int_distribution<size_t> cosmic_dist(0,cosmic_options.size()-1);
             powers.cosmic=cosmic_options[cosmic_dist(gen)];
         }else{
-            powers.basic=user_input("What would you like your character's basic power to be?",basic_powers);
-            powers.alignment=user_input("What would you like your character's alignment to be?",alignments);
-            powers.cosmic=user_input("What would you like your character's cosmic power to be?",cosmic_powers);
+            powers.basic=input("What would you like your character's basic power to be?",basic_powers);
+            powers.alignment=input("What would you like your character's alignment to be?",alignments);
+            powers.cosmic=input("What would you like your character's cosmic power to be?",cosmic_powers);
         }
         std::vector<std::string> basic_starter_moves={"flame_burst", "iron_spike", "splinter", "pebble_shot", "water_jet"};
         auto basic_it=std::find(basic_powers.begin(),basic_powers.end(),powers.basic);
@@ -265,23 +265,23 @@ bool battle_loop(bool turn,player& player,enemy& enemy){
     if(turn){
         string action="see moves";
         while(action=="see moves"){
-            action=user_input(string("What would you like to do?"),{"use move", "see moves"});
+            action=input(string("What would you like to do?"),{"use move", "see moves"});
             available_moves.clear();
             for(size_t i=0;i<player.known_moves.size();++i){if(player.cooldown_times.find(player.known_moves[i])==player.cooldown_times.end()){available_moves.push_back(player.known_moves[i]);}}
             if(action=="see moves"){
-                typewrite("You can use the following moves: \n");
+                print("You can use the following moves: \n");
                 for(size_t i=0;i<available_moves.size();++i){
                     string cooldown;
                     if(moves.at(available_moves[i]).cooldown==1){cooldown="no";}
                     else{cooldown=std::to_string(moves.at(available_moves[i]).cooldown-1)+"-turn";}
-                    typewrite(moves.at(available_moves[i]).name+": "+std::to_string(moves.at(available_moves[i]).damage)+" damage, "+cooldown+" cooldown \n");
+                    print(moves.at(available_moves[i]).name+": "+std::to_string(moves.at(available_moves[i]).damage)+" damage, "+cooldown+" cooldown \n");
                 }
             }else{
                 unordered_map<string,string> move_lookup;
                 for(const auto& move_id:available_moves){move_lookup[moves.at(move_id).name]=move_id;}
                 vector<string> available_move_names;
                 for(const auto& pair:move_lookup){available_move_names.push_back(pair.first);}
-                string move_to_use=user_input("What move would you like to use?",{available_move_names});
+                string move_to_use=input("What move would you like to use?",{available_move_names});
                 string move_to_use_id=move_lookup[move_to_use];
                 int damage=get_damage(true,player.cooldown_times,player.stats,enemy.stats,enemy.powers,move_to_use_id);
                 enemy.stats.health-=damage;
@@ -301,7 +301,7 @@ bool battle_loop(bool turn,player& player,enemy& enemy){
                         else{enemy.memory.cosmic="axiom";}
                     }
                 }
-                typewrite("You used "+moves.at(move_to_use_id).name+" and dealt "+std::to_string(damage)+" damage to Bob! \n");
+                print("You used "+moves.at(move_to_use_id).name+" and dealt "+std::to_string(damage)+" damage to "+enemy.name+"! \n");
             }
         }
         for(auto it=player.cooldown_times.begin();it!=player.cooldown_times.end();){
@@ -323,7 +323,7 @@ bool battle_loop(bool turn,player& player,enemy& enemy){
         const int actual_damage=get_damage(true,enemy.cooldown_times,enemy.stats,player.stats,player.powers,best_move);
         const int expected_damage=get_damage(false,enemy.cooldown_times,enemy.stats,player.stats,enemy.memory,best_move);
         player.stats.health-=actual_damage;
-        typewrite("Bob used "+moves.at(best_move).name+" and dealt "+std::to_string(actual_damage)+" damage to you! \n"+"You have "+std::to_string(player.stats.health)+" health remaining. \n");
+        print(enemy.name+" used "+moves.at(best_move).name+" and dealt "+std::to_string(actual_damage)+" damage to you! \n"+"You have "+std::to_string(player.stats.health)+" health remaining. \n");
         if(actual_damage!=expected_damage){
             if(moves.at(best_move).level=="basic"){enemy.memory.basic=player.powers.basic;}
             else if(moves.at(best_move).level=="alignment"){enemy.memory.alignment=player.powers.alignment;}
@@ -337,16 +337,29 @@ bool battle_loop(bool turn,player& player,enemy& enemy){
     }
     return !turn;
 }
+string hp_print(int player_health,int enemy_health,string enemy_name){
+    if(enemy_health<=0){return("You have defeated "+enemy_name+"! \n");}
+    else if(player_health<=0){return("You have been defeated by "+enemy_name+". \n");}
+    else{
+        if(static_cast<double>(enemy_health-player_health)/player_health>0.0){
+            return(enemy_name+" has "+std::to_string(static_cast<int>(std::round((static_cast<double>(enemy_health-player_health)/player_health)*10000)/100))+"% more health than you. \n");
+        }else if(static_cast<double>(enemy_health-player_health)/player_health<0.0){
+            return(enemy_name+" has "+std::to_string(static_cast<int>(0-std::round((static_cast<double>(enemy_health-player_health)/player_health)*10000)/100))+"% less health than you. \n");
+        }else{
+            return(enemy_name+" has the same health as you. \n");
+        }
+    }
+}
 int main(){
-    typewrite("Welcome to the game! You are a player in a world of magic and adventure. You will be able to choose your character's stats and powers, and then embark on a journey to defeat the evil forces that threaten the land. \n");
+    print("Welcome to the game! You are a player in a world of magic and adventure. You will be able to choose your character's stats and powers, and then embark on a journey to defeat the evil forces that threaten the land. \n");
     player player;
-    typewrite(player.name+" has been created with the following stats: \n");
-    typewrite("Attack: "+std::to_string(player.stats.attack)+" \n");
-    typewrite("Speed: "+std::to_string(player.stats.speed)+" \n");
-    typewrite("Defense: "+std::to_string(player.stats.defense)+" \n");
-    typewrite("Health: "+std::to_string(player.stats.health)+" \n");
-    typewrite(get_lore(player)+"\n");
-    typewrite("You are now ready to embark on your journey. Good luck, and may the forces of magic be with you! \n");
+    print(player.name+" has been created with the following stats: \n");
+    print("Attack: "+std::to_string(player.stats.attack)+" \n");
+    print("Speed: "+std::to_string(player.stats.speed)+" \n");
+    print("Defense: "+std::to_string(player.stats.defense)+" \n");
+    print("Health: "+std::to_string(player.stats.health)+" \n");
+    print(get_lore(player)+"\n");
+    print("You are now ready to embark on your journey. Good luck, and may the forces of magic be with you! \n");
     enemy bob{"bob",{10,10,10,100},{},{},{},{},{}};
     std::uniform_int_distribution<size_t> basic_dist(0,basic_powers.size()-1);
     bob.powers.basic=basic_powers[basic_dist(gen)];
@@ -370,19 +383,9 @@ int main(){
         }
     }
     bool turn=true;
-    typewrite("You have encountered Bob! Prepare for battle!\nYou have "+std::to_string(player.stats.speed)+" SPD, "+std::to_string(player.stats.attack)+" ATK, "+std::to_string(player.stats.defense)+" DFN, "+std::to_string(player.stats.health)+" HLT\n");
+    print("You have encountered "+bob.name+"! Prepare for battle!\nYou have "+std::to_string(player.stats.speed)+" SPD, "+std::to_string(player.stats.attack)+" ATK, "+std::to_string(player.stats.defense)+" DFN, "+std::to_string(player.stats.health)+" HLT\n");
     while(player.stats.health>0&&bob.stats.health>0){
         turn=battle_loop(turn,player,bob);
-        if(bob.stats.health<=0){typewrite("You have defeated Bob! \n");}
-        else if(player.stats.health<=0){typewrite("You have been defeated by Bob. \n");}
-        else{
-            if(static_cast<double>(bob.stats.health-player.stats.health)/player.stats.health>0.0){
-                typewrite("Bob has "+std::to_string(static_cast<int>(std::round((static_cast<double>(bob.stats.health-player.stats.health)/player.stats.health)*10000)/100))+"% more health than you. \n");
-            }else if(static_cast<double>(bob.stats.health-player.stats.health)/player.stats.health<0.0){
-                typewrite("Bob has "+std::to_string(static_cast<int>(0-std::round((static_cast<double>(bob.stats.health-player.stats.health)/player.stats.health)*10000)/100))+"% less health than you. \n");
-            }else{
-                typewrite("Bob has the same health as you. \n");
-            }
-        }
+        print(hp_print(player.stats.health,bob.stats.health,bob.name));
     }
 }
