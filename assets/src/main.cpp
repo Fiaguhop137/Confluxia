ec23ea3fef2f23416537f129c7789e4224cfa014
Fiaguhop137 <eastonzou@gmail.com>
2026-09-02 17:23:17 -0400

    Auto-commit: 2026.09.02.17.23.17

--- assets/src/main.cpp
+++ assets/src/main.cpp
@@ -1,17 +1,28 @@
+#include <iostream>
 #include <fstream>
 #include <string>
-#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <filesystem>
 #include "desktop.hpp"
-int main(){
-    std::ifstream license("LICENSE");
-    std::string line;
-    bool licensed=false;
-    while(std::getline(license, line)){
-        if(line.find("Fiaguhop137")!=std::string::npos){
-            licensed=true;
-            break;
+std::uint64_t hash_file(const std::string& filename) {
+    std::uint64_t hash=0xcbf29ce484222325;
+    constexpr std::uint64_t prime=0x100000001b3;
+    std::ifstream file(filename,std::ios::binary);
+    if(file.is_open()&&std::filesystem::file_size(filename)<1280){
+        std::vector<char> bytes(std::filesystem::file_size(filename));
+        file.read(bytes.data(),bytes.size());
+        for (char ch:bytes){
+            hash^=static_cast<std::uint8_t>(ch);
+            hash*=prime;
         }
     }
+    return hash;
+}
+int main(){
+    std::string line;
+    bool licensed=false;
+    if(hash_file("LICENSE")==0xc3f09ce3a24fbeab){licensed=true;}
     if(licensed){
         desktop::run();
     }else{
